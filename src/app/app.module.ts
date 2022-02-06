dbd47107cedfa06ae5d6abfb5287aa40410bc938
vladernn <vladys.3234@gmail.com>
2022-02-06 14:23:21 +0100

    use atomic design structure for UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './UI/material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeModule } from './UI/view-models/home/home.module';
+import { HomeModule } from './UI/pages/home/home.module';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginGateway } from './domain/models/login/gateway/login.gateway';
 import { LoginApiService } from './infrastructure/driven-adapter/login-api/login-api.service';
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
-      import('./UI/view-models/home/home.module').then((m) => m.HomeModule),
+      import('./UI/pages/home/home.module').then((m) => m.HomeModule),
   },
   // { path: '', redirectTo: '/home' },
   // { path: '**', component: PageNotFoundComponent },
