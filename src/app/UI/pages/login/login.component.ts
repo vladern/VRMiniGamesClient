47067e4254ac3757609064857d8165d01f8689b2
vladyslav kuchmenko <vkuchmenko@neuromobilemarketing.com>
2022-08-08 18:57:20 +0200

    remove component store frome code

--- src/app/UI/pages/login/login.component.ts
+++ src/app/UI/pages/login/login.component.ts
@@ -2,25 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { LoginUseCase } from 'src/app/domain/use-cases/login.use-case';
-import { LoginStore } from '../../store/login/login.store';
 
 @Component({
   selector: 'vrmg-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [LoginStore]
 })
 export class LoginComponent implements OnInit {
 
   public loginFormGroup$: Observable<FormGroup>;
 
   constructor(
-    private readonly loginUseCase: LoginUseCase,
-    private readonly loginStore: LoginStore
+    private readonly loginUseCase: LoginUseCase
   ) { }
 
   ngOnInit(): void {
-    this.loginFormGroup$ = this.loginStore.loginFormGroup$;
   }
 
   public submit({email, password}): void {
