e96277fa0f9e7dab65bce78b54c65a5130fc08d3
vladyslav kuchmenko <vkuchmenko@neuromobilemarketing.com>
2022-08-18 16:43:25 +0200

    implement login api

--- src/app/infrastructure/driven-adapter/graph-ql-login-api/graph-ql-login-api.service.spec.ts
+++ src/app/infrastructure/driven-adapter/graph-ql-login-api/graph-ql-login-api.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  ApolloTestingModule,
+  ApolloTestingController,
+} from 'apollo-angular/testing';
 import { GraphQLLoginApiService } from './graph-ql-login-api.service';
 
-describe('LiginApiService', () => {
+fdescribe('LiginApiService', () => {
   let service: GraphQLLoginApiService;
+  let controller: ApolloTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ApolloTestingModule],
+    });
     service = TestBed.inject(GraphQLLoginApiService);
+    controller = TestBed.inject(ApolloTestingController);
+  });
+
+  afterEach(() => {
+    controller.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#login should return expected LoginResponse (Apollo should be called once)', (done: DoneFn) => {
+    const loginInput = {
+      email: 'test@example.com',
+      password: 'password',
+    };
+    const expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmUzMGRiNTNkOGQzNDc3Y2IxNGI2MyIsImVtYWlsIjoidmxhZHlzLjMyMzRAZ21haWwuY29tIiwiaWF0IjoxNjYwODI1ODYxLCJleHAiOjE2NjA5MTIyNjF9.KVHe_GN89gl-oS_8kSWKE03x108o-4JAWL4GmLGVbiU";
+    service.login(loginInput.email, loginInput.password).subscribe(loginResponse => {
+      expect(loginResponse.token).toBe(expectedToken);
+      done();
+    });
+    const op = controller.expectOne(service.loginQuery);
+    expect(op.operation.variables.data).toEqual(loginInput);
+    op.flush({
+      data: {
+        login: {
+          token: expectedToken
+        }
+      }
+    });
   });
 });
--- src/app/infrastructure/driven-adapter/graph-ql-login-api/graph-ql-login-api.service.ts
+++ src/app/infrastructure/driven-adapter/graph-ql-login-api/graph-ql-login-api.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from '@angular/core';
 import { LoginResponse } from 'src/app/domain/models/login/login-response.model';
 import { LoginGateway } from 'src/app/domain/models/login/gateway/login.gateway';
 import { Observable } from 'rxjs';
+import { Apollo, gql } from 'apollo-angular';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GraphQLLoginApiService extends LoginGateway {
+  public loginQuery = gql`
+    query login($data: LoginInput) {
+      login(data: $data) {
+        token
+      }
+    }
+  `;
 
-  constructor() {
+  constructor(private apollo: Apollo) {
     super();
   }
 
   public login(email: string, password: string): Observable<LoginResponse> {
-    // TODO: Implement here comunication with GraphQL
-    throw new Error('Not implemented yet!!');
+    return this.apollo
+      .watchQuery<any>({
+        query: this.loginQuery,
+        variables: {
+          data: { email, password },
+        },
+      })
+      .valueChanges.pipe(shareReplay(1), map((response) => ({ ...response?.data?.login } as LoginResponse)));
   }
 }
