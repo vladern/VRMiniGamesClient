0bb15d74692a65eb4b7df8614e7e0164bd68b246
vladyslav kuchmenko <vkuchmenko@neuromobilemarketing.com>
2022-08-24 11:12:24 +0200

    refactor not used code

--- src/app/UI/common/components/atoms/input/input.component.ts
+++ src/app/UI/common/components/atoms/input/input.component.ts
@@ -1,44 +1,20 @@
-import { Component, EventEmitter, forwardRef, Input, Output, ViewChild } from '@angular/core';
-import { ControlValueAccessor, DefaultValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'vrmg-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputComponent),
-      multi: true
-    }
-  ]
 })
-export class InputComponent implements ControlValueAccessor {
+export class InputComponent {
   @Input() label: string;
   @Input() type: string;
   @Input() placeholder: string;
   @Input() value: string | number;
   @Output() valueChange: EventEmitter<string | number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   onValueChange(value: string | number): void {
     this.valueChange.emit(value);
-  }
-
-
-  onChange = event => {};
-  onTouched = () => {};
-
-  writeValue(value: any) {
-    this.value = value;
-  }
-
-  registerOnChange(fn: any) {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any) {
-    this.onTouched = fn;
   }
 }
