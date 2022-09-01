8f5c954b5ddcac6e0ba918c9c7fb6908c2213ec9
vladyslav kuchmenko <vkuchmenko@neuromobilemarketing.com>
2022-09-01 13:47:59 +0200

    add card component

---
+++ src/app/UI/common/components/atoms/card/card.component.spec.ts
@@ -0,0 +1,51 @@
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { MaterialModule } from 'src/app/UI/material-module';
+import { CardComponent } from './card.component';
+
+@Component({
+  template: `<vrmg-card><h1>Hello world</h1></vrmg-card>`,
+})
+class TestHostComponent {}
+
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardComponent, TestHostComponent ],
+      imports: [ MaterialModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardComponent);
+    component = fixture.componentInstance; 
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should has the title "Hello world"', () => {
+    const hostElement: HTMLElement = fixture.nativeElement;
+    const titleElement: HTMLElement = hostElement.querySelector('.qa-title')!;
+    component.title = 'Hello world';
+    fixture.detectChanges();
+    expect(titleElement.textContent).toEqual('Hello world');
+  });
+
+  it('should has content with "Hello world"', () => {
+    const testFixture = TestBed.createComponent(TestHostComponent);
+    const debugElement: DebugElement = testFixture.debugElement.query(
+      By.css('h1')
+    );
+    const element: Element = debugElement.nativeElement;
+    expect(element.textContent).toBe('Hello world');
+  });
+});
--- src/app/UI/common/components/index.ts
+++ src/app/UI/common/components/index.ts
@@ -1,5 +1,11 @@
 import { ButtonComponent } from './atoms/button/button.component';
+import { CardComponent } from './atoms/card/card.component';
 import { ErrorComponent } from './atoms/error/error.component';
 import { InputComponent } from './atoms/input/input.component';
 
-export const index = [ButtonComponent, InputComponent, ErrorComponent];
+export const index = [
+  ButtonComponent,
+  InputComponent,
+  ErrorComponent,
+  CardComponent,
+];
